df0c76e0b44b36c4b84ef3b48c96ce2aed2adafe
normalplayar <chrissh37@gmail.com>
2023-11-27 21:12:42 +0700

    a

--- Alien_invasion/ship.py
+++ Alien_invasion/ship.py
@@ -5,13 +5,13 @@ class Ship():
         """Initialize the ship and set its starting position."""
         self.screen = screen
         # Load the ship image and get its rect.
-        self.image = pygame.image.load("C:\Users\chris\Documents\GitHub\Alien_invasion\Alien_invasion\images")
+        self.image = pygame.image.load("C:/Users/chris/Documents/GitHub/Alien_invasion/Alien_invasion/images/ship.bmp")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         # Start each new ship at the bottom center of the screen.
         self.rect.bottom = self.screen_rect.bottom
         self.rect.centerx = self.screen_rect.centerx
 
-def blitme(self):
-    """Draw the ship at its current location."""
-    self.screen.blit(self.image, self.rect)
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
