2df7a45d6ee8cc53813d8bc476f884cfaff5d49f
normalplayar <chrissh37@gmail.com>
2023-11-27 21:02:38 +0700

    a

--- alien_invasion/chpt10.py
+++ alien_invasion/chpt10.py
@@ -1,12 +1,14 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
@@ -14,7 +16,7 @@ def run_game():
                 sys.exit()
         
         screen.fill(ai_settings.bg_color)
-
+        ship.blitme()
         pygame.display.flip()
 
 run_game()
